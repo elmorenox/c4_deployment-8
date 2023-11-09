5f086616def9086aef6ff4d07de4226a56bd3538
Ethan Arteta <129544035+ethanarteta@users.noreply.github.com>
2023-11-09 17:25:56 -0600

    Update (infra)main.tf

--- TerraformFiles/(infra)main.tf
+++ TerraformFiles/(infra)main.tf
@@ -121,7 +121,7 @@ resource "aws_ecs_task_definition" "frontend_task" {
   container_definitions = jsonencode([
     {
       name  = "frontend-container"
-      image = "your_registry/frontend:latest" # Replace with your Docker image repository URL
+      image = "kha1i1/deployment8:FE_image"
       
       # Commands to run inside the container
       command = [
