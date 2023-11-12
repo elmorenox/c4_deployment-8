096d0d4ca3d2f506e0cede0f7b77390621b0505c
Luis Moreno <x.luismoreno@gmail.com>
2023-11-11 20:27:22 -0500

    jenkinsfile be

--- TerraformFiles/VPC.tf
+++ TerraformFiles/VPC.tf
@@ -40,10 +40,6 @@ resource "aws_route" "internet_gateway_route" {
   gateway_id             = aws_internet_gateway.D8Gateway.id
 }
 
-resource "aws_eip" "elastic-ip" {
-  domain = "vpc"
-}
-
   # Create route table
 resource "aws_route_table" "dep8_route_table" {
   vpc_id = aws_vpc.dep8_vpc.id
